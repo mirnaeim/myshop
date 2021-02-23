657b1132314d6792630e0f637edaccc4efa4f092
Naeim <mirnaimhoseyni@gmail.com>
2021-02-23 20:14:53 +0330

    Add sending email for orders

--- myshop/settings.py
+++ myshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import my_secrets
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,8 +126,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mirnaimhoseyni@gmail.com'
+EMAIL_HOST_PASSWORD = my_secrets.password
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
--- orders/tasks.py
+++ orders/tasks.py
@@ -5,6 +5,11 @@ from .models import Order
 
 @task
 def order_created(order_id):
+    """
+    Task to send an e-mail notification when an order is
+    successfully created.
+    """
+
     order = Order.objects.get(id=order_id)
     subject = f'Order nr. {order.id}'
     message = f'Dear {order.first_name},\n\n' \
@@ -12,6 +17,6 @@ def order_created(order_id):
         f'Your order ID is {order.id}.'
     mail_sent = send_mail(subject,
                           message,
-                          'admin@myshop.com',
+                          'mirnaimhoseyni@gmail.com',
                           [order.email])
     return mail_sent
