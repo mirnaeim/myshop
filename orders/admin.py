3b068fb90e8073c48b0a171c0c37ea984dc9c2ec
Naeim <mirnaimhoseyni@gmail.com>
2021-02-27 23:27:13 +0330

    costum view in administration site

--- orders/admin.py
+++ orders/admin.py
@@ -3,7 +3,8 @@ from .models import Order, OrderItem
 import csv
 import datetime
 from django.http import HttpResponse
-
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 def export_to_csv(modeladmin, request, queryset):
     opts = modeladmin.model._meta
@@ -32,11 +33,16 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    'created', 'updated']
+                    'created', 'updated', order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
