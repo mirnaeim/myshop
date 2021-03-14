65d804e8379c3b6544ea7eb6ba00fdf689fe8b15
mirnaeim <mirnaimhoseyni@gmail.com>
2021-03-14 23:09:34 +0330

    Installed rosetta

--- myshop/settings.py
+++ myshop/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
+    # Third Party
+    'rosetta',
+
+
     #Local
     'shop.apps.ShopConfig',
     'cart.apps.CartConfig',
